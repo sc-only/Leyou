f99ddb53829b9f403e99dba7d99f9b193dddccb9
cccr <1781587589@qq.com>
2019-12-21 19:11:17 +0800

    整合vue前端工程，实现查询商品类别的功能

---
+++ code/programe/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+
+    /**
+     * 通过pid查询
+     * @param pid
+     * @return
+     */
+    public List<Category> queryCategoryByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categoryMapper.select(record);
+    }
+}
