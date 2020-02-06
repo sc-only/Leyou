a82714bd259ad437ae70edd8ae78ae765aab7eba
cccr <1781587589@qq.com>
2020-02-06 20:44:03 +0800

    完成商品管理的商品列表页面的查询分页功能

--- code/programe/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
+++ code/programe/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -1,7 +1,9 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.additional.idlist.SelectByIdListMapper;
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.ids.SelectByIdsMapper;
 
-public interface CategoryMapper extends Mapper<Category> {
+public interface CategoryMapper extends Mapper<Category>, SelectByIdListMapper<Category,Long> {
 }
---
+++ code/programe/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
@@ -0,0 +1,77 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.bo.SpuBo;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.mapper.SpuDetailMapper;
+import com.leyou.item.mapper.SpuMapper;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.pojo.Spu;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GoodsService {
+    @Autowired
+    private SpuMapper spuMapper;
+
+    @Autowired
+    private SpuDetailMapper spuDetailMapper;
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据条件分页查询Spu
+     * @param key
+     * @param saleable
+     * @param page
+     * @param rows
+     * @return
+     */
+    public PageResult<SpuBo> querySpuByPage(String key, Boolean saleable, Integer page, Integer rows) {
+
+        Example example = new Example(Spu.class);
+        Example.Criteria criteria = example.createCriteria();
+        //TODO::添加查询条件
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("title","%"+key+"%");
+        }
+        //TODO::添加上下架的过滤条件
+        if(saleable!=null){
+            criteria.andEqualTo("saleable",saleable);
+        }
+        //TODO::添加分页
+        PageHelper.startPage(page,rows);
+        //TODO::执行查询，获取spu集合
+        List<Spu> spus = this.spuMapper.selectByExample(example);
+        PageInfo<Spu> pageInfo = new PageInfo<>(spus);
+        //TODO::spu集合转化为spubo集合
+        List<SpuBo> spuBos = spus.stream().map(spu -> {
+            SpuBo spuBo = new SpuBo();
+            BeanUtils.copyProperties(spu, spuBo);
+            //TODO:: 查询品牌名称
+            Brand brand = this.brandMapper.selectByPrimaryKey(spu.getBrandId());
+            spuBo.setBname(brand.getName());
+            //TODO:: 查询分类名称
+            List<String> names = this.categoryService.queryNameByIds(Arrays.asList(spu.getCid1(), spu.getCid2(), spu.getCid3()));
+            spuBo.setCname(StringUtils.join(names, "~"));
+            return spuBo;
+        }).collect(Collectors.toList());
+        //TODO::返回pageResult<SpuBo>
+        return new PageResult<>(pageInfo.getTotal(),spuBos);
+    }
+}
